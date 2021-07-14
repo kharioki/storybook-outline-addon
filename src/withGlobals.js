4ae9f34750e1355970f1920590ec6a7b4f32eb08
tony <antolisah@gmail.com>
2021-07-14 08:14:34 +0300

    tracking global state and decorators

--- src/withGlobals.js
+++ src/withGlobals.js
@@ -1,39 +1,36 @@
-import { useEffect, useGlobals } from "@storybook/addons";
-export const withGlobals = (StoryFn, context) => {
-  const [{ myAddon }] = useGlobals(); // Is the addon being used in the docs panel
+import { useEffect, useMemo, useGlobals } from "@storybook/addons";
 
+import { clearStyles, addOutlineStyles } from './helpers';
+import outlineCSS from './outlineCSS';
+
+export const withGlobals = (StoryFn, context) => {
+  const [{ outlineActive }, updateGlobals] = useGlobals(); 
+
+  // Is the addon being used in the docs panel
   const isInDocs = context.viewMode === "docs";
+
+  const outlineStyles = useMemo(() => {
+    const selector = isInDocs ? `#anchor--${context.id} .docs-story` : '.sb-show-main';
+
+    return outlineCSS(selector);
+  }, [context.id]);
+
   useEffect(() => {
-    // Execute your side effect here
-    // For example, to manipulate the contents of the preview
     const selectorId = isInDocs
-      ? `#anchor--${context.id} .docs-story`
-      : `#root`;
-    displayToolState(selectorId, {
-      myAddon,
-      isInDocs,
-    });
-  }, [myAddon]);
+      ? `addon-outline-docs-${context.id}` 
+      : `addon-outline`;
+    
+    if(!outlineActive) {
+      clearStyles(selectorId);
+      return;
+    }
+
+    addOutlineStyles(selectorId, outlineStyles);
+
+    return () => {
+      clearStyles(selectorId);
+    };
+  }, [outlineActive, outlineStyles, context.id]);
+
   return StoryFn();
 };
-
-function displayToolState(selector, state) {
-  const rootElement = document.querySelector(selector);
-  let preElement = rootElement.querySelector("pre");
-
-  if (!preElement) {
-    preElement = document.createElement("pre");
-    preElement.style.setProperty("margin-top", "2rem");
-    preElement.style.setProperty("padding", "1rem");
-    preElement.style.setProperty("background-color", "#eee");
-    preElement.style.setProperty("border-radius", "3px");
-    preElement.style.setProperty("max-width", "600px");
-    rootElement.appendChild(preElement);
-  }
-
-  preElement.innerText = `This snippet is injected by the withGlobals decorator.
-It updates as the user interacts with the ⚡ tool in the toolbar above.
-
-${JSON.stringify(state, null, 2)}
-`;
-}
